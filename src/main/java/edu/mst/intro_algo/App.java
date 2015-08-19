32fa1d41bfb5f1f9013b257921a944cca1dceac1
Martin Sturm <martin.sturm@radiantminds.com>
2015-08-19 20:53:29 +1000

    maven init

---
+++ src/main/java/edu/mst/intro_algo/App.java
@@ -0,0 +1,13 @@
+package edu.mst.intro_algo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
